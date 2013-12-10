c177715a3958e3558582bb0d9c3c9bf976cb35e0
Dylan <pumped@Dylan-Desktop.(none)>
2013-12-10 12:54:37 +1000

    Editor modes working, playback bar

--- js/interface.js
+++ js/interface.js
@@ -8,6 +8,8 @@ $('document').ready(function(){
     	$(window.location.hash).click();
 	}
 
+	setupTimeline();
+
 });
 
 function setMode(mode) {
@@ -15,12 +17,32 @@ function setMode(mode) {
 	$('.navbar-nav li').removeClass('active');
 	$('#'+mode).parent().addClass('active');
 
+	console.log('changed mode')
+
 	//set ui
 	if (mode == 'edit') {
-
+		mc.editMode();
 	} else if (mode == 'zone') {
-
+		mc.zoneMode();
 	} else if (mode == 'plan') {
-
+		mc.planMode();
+	} else if (mode == 'overview') {
+		mc.overviewMode();
 	}
+}
+
+function setupTimeline() {
+	$('.slider').slider({
+		min:2000,
+		max:2050,
+		step:1,
+		value: 2013,
+		orientation: 'horizontal'
+	});
+}
+
+function log(m) {
+	console.log(m);
+	d = new Date().toLocaleTimeString();
+	$('.console').prepend('<i>'+d+'</i>: '+m+'<br/>');
 }
--- js/map.js
+++ js/map.js
@@ -2,32 +2,300 @@ var map;
 var tiled;
 
 $('document').ready(function(){
-    setupMap();
+    mc = new MapController();
+    mc.setupMap();
+    log('Map Initialised');
 });
 
-function setupMap() {
+function MapController() {
+    this.mode = 'edit';
+}
+
+MapController.prototype.editMode = function() {
+    this.mode = 'edit';
+
+    $('.customEditingToolbar').show();
+    $('[class^=olControlDrawFeaturePoint]').show();
+    $('[class^=olControlDrawFeaturePath]').show();
+    $('#timeline').hide();
+
+    //hide layers
+    this.wfstPoint.setVisibility(true);
+    this.wfstLine.setVisibility(true);
+    this.wfstPolygon.setVisibility(false);
+    this.tiled.setVisibility(false);
+}
+
+MapController.prototype.overviewMode = function() {
+    this.mode = 'overview';
+
+    $('.customEditingToolbar').hide();
+    $('#timeline').hide();
+
+    //hide layers
+    this.wfstPoint.setVisibility(false);
+    this.wfstLine.setVisibility(false);
+    this.wfstPolygon.setVisibility(false);
+    this.tiled.setVisibility(true);
+}
+
+MapController.prototype.planMode = function() {
+    this.mode = 'plan';
+
+    $('.customEditingToolbar').hide();
+    $('#timeline').show();
+
+    //hide layers
+    this.wfstPoint.setVisibility(false);
+    this.wfstLine.setVisibility(false);
+    this.wfstPolygon.setVisibility(false);
+    this.tiled.setVisibility(true);
+}
+
+MapController.prototype.zoneMode = function() {
+    this.mode = 'zone';
+
+    $('.customEditingToolbar').show();
+    $('[class^=olControlDrawFeaturePoint]').hide();
+    $('[class^=olControlDrawFeaturePath]').hide();
+    $('#timeline').hide();
+
+    //hide layers
+    this.wfstPoint.setVisibility(false);
+    this.wfstLine.setVisibility(false);
+    this.wfstPolygon.setVisibility(true);
+    this.tiled.setVisibility(true);
+}
+
+
+//setup map function
+MapController.prototype.setupMap = function() {
     OpenLayers.ProxyHost = "proxy.php?url=";
 
-    map = new OpenLayers.Map({
-        div: "map",
-        projection: new OpenLayers.Projection("EPSG:900913")
-    });
-    
+    var featureserver = "http://115.146.85.81:9090/"
+
+    OpenLayers.Util.extend(OpenLayers.Request,{
+                           makeSameOrigin : function(url, proxy) {
+                           return proxy + encodeURIComponent(url);
+                           }
+                           });
+
+
+    var DeleteFeature = OpenLayers.Class(OpenLayers.Control.SelectFeature, {
+                                         clickFeature: function(feature) {
+                                         if(feature.fid == undefined) {
+                                         feature.layer.destroyFeatures([feature]);
+                                         } else {
+                                         feature.state = OpenLayers.State.DELETE;
+                                         feature.layer.events.triggerEvent("afterfeaturemodified",
+                                                                        {feature: feature});
+                                         feature.renderIntent = "select";
+                                         feature.layer.drawFeature(feature);
+                                         }
+                                         },
+                                         clickoutFeature: function(feature) {},
+                                         unselectAll: function(options) {
+                                         },
+                                         toggleSelect: function() {},
+                                         overFeature: function(feature) {
+                                         var layer = feature.layer;
+                                         if(feature.state != OpenLayers.State.DELETE) {
+                                         return OpenLayers.Control.SelectFeature.prototype.overFeature.apply(this, arguments);
+                                         }
+                                         },
+                                         outFeature : function(feature) {
+                                         if(feature.state != OpenLayers.State.DELETE) {
+                                         return OpenLayers.Control.SelectFeature.prototype.outFeature.apply(this, arguments);
+                                         }
+                                         },
+                                         CLASS_NAME: "OpenLayers.Control.DeleteFeature"
+                                         });
+
+
+
+
+    var map = new OpenLayers.Map('map', {
+                                 units : 'm',
+                                 projection: new OpenLayers.Projection("EPSG:4326"),
+                                 displayProjection: new OpenLayers.Projection("EPSG:4326")
+                                 });
+
     //base layers
     var osm = new OpenLayers.Layer.OSM("OSM");
     var gmap = new OpenLayers.Layer.Google("Google", {type: 'styled'});          
     var gsat = new OpenLayers.Layer.Google("Satellite",
         {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 22});
 
-    //set up a save strategy
-    /*var saveStrategy = new OpenLayers.Strategy.Save();
-    saveStrategy.events.register("success", '', function(){console.info('saved')});
-    saveStrategy.events.register("failure", '', function(){console.error('save failed')});*/
 
-    // map extent
-    //var mapextent = new OpenLayers.Bounds(142.0, 16.0, 150.0, 23.0).transform(WGS84, WGS84_google_mercator);
+    var savePoint = new OpenLayers.Strategy.Save();
+    var wfstPoint = this.wfstPoint = new OpenLayers.Layer.Vector("Points", {
+                                                strategies: [new OpenLayers.Strategy.BBOX(), savePoint],
+                                                projection: new OpenLayers.Projection("EPSG:4326"),
+                                                protocol: new OpenLayers.Protocol.WFS({
+                                                                                      version: "1.1.0",
+                                                                                      srsName: "EPSG:4326",
+                                                                                      url: featureserver + "?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&maxfeatures=10000&TYPENAME=siam",
+                                                                                      //featurePrefix: "fs",
+                                                                                      featureType: "siam",
+                                                                                      featureNS :  "http://featureserver.org/fs",
+                                                                                      geometryName: "geometry"
+                                                                                      //schema: featureserver + "?SERVICE=WFS&REQUEST=DescribeFeatureType?VERSION=1.1.0&TYPENAME=fs_point"
+                                                                                      })
+                                                });
+    wfstPoint.id = "point";
+    map.addLayer(wfstPoint);
 
-    /*tiled = new OpenLayers.Layer.WMS(
+    var saveLine = new OpenLayers.Strategy.Save();
+    var wfstLine = this.wfstLine = new OpenLayers.Layer.Vector("Lines", {
+                                               strategies: [new OpenLayers.Strategy.BBOX(), saveLine],
+                                               projection: new OpenLayers.Projection("EPSG:4326"),
+                                               protocol: new OpenLayers.Protocol.WFS({
+                                                                                     version: "1.1.0",
+                                                                                     srsName: "EPSG:4326",
+                                                                                     url: featureserver + "?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=test",
+                                                                                     //featurePrefix: "fs",
+                                                                                     featureType: "fs_line",
+                                                                                     featureNS :  "http://featureserver.org/fs",
+                                                                                     geometryName: "geometry"
+                                                                                     //schema: featureserver + "?SERVICE=WFS&REQUEST=DescribeFeatureType?VERSION=1.1.0&TYPENAME=fs_line"
+                                                                                     })
+                                               });
+    wfstLine.id = "line";
+    map.addLayer(wfstLine);
+
+    var savePolygon = new OpenLayers.Strategy.Save();
+    var wfstPolygon = this.wfstPolygon = new OpenLayers.Layer.Vector("Zones", {
+                                                 strategies: [new OpenLayers.Strategy.BBOX(), savePolygon],
+                                                 projection: new OpenLayers.Projection("EPSG:4326"),
+                                                 protocol: new OpenLayers.Protocol.WFS({
+                                                                                       version: "1.1.0",
+                                                                                       srsName: "EPSG:4326",
+                                                                                       url: featureserver + "?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=zone",
+                                                                                       //featurePrefix: "fs",
+                                                                                       featureNS :  "http://featureserver.org/fs",
+                                                                                       featureType: "fs_polygon",
+                                                                                       geometryName: "geometry"
+                                                                                       //schema: featureserver + "?SERVICE=WFS&REQUEST=DescribeFeatureType?VERSION=1.1.0&TYPENAME=fs_polygon"
+                                                                                       })
+                                                 });
+    wfstPolygon.id = "polygon";
+    map.addLayer(wfstPolygon);
+
+
+    var panel = new OpenLayers.Control.Panel({
+                                             displayClass: 'customEditingToolbar',
+                                             allowDepress: true
+                                             });
+
+    var point = new OpenLayers.Control.DrawFeature(
+                                                   wfstPoint, OpenLayers.Handler.Point,
+                                                   {
+                                                   title: "Draw Point",
+                                                   displayClass: "olControlDrawFeaturePoint",
+                                                   multi: false
+                                                   }
+                                                   );
+
+    var path = new OpenLayers.Control.DrawFeature(
+                                                  wfstLine, OpenLayers.Handler.Path,
+                                                  {
+                                                  title: "Draw Path",
+                                                  displayClass: "olControlDrawFeaturePath",
+                                                  multi: false
+                                                  }
+                                                  );
+
+    var polygon = new OpenLayers.Control.DrawFeature(
+                                                     wfstPolygon, OpenLayers.Handler.Polygon,
+                                                     {
+                                                     title: "Draw Polygon",
+                                                     displayClass: "olControlDrawFeaturePolygon",
+                                                     multi: false
+                                                     }
+                                                     );
+
+    var editPoint = new OpenLayers.Control.ModifyFeature(wfstPoint, {
+                                                         id:"point",
+                                                         standalone:true,
+                                                         mode: OpenLayers.Control.ModifyFeature.DRAG// | OpenLayers.Control.ModifyFeature.RESHAPE | OpenLayers.Control.ModifyFeature.RESIZE | OpenLayers.Control.ModifyFeature.ROTATE
+                                                         });
+    var editLine= new OpenLayers.Control.ModifyFeature(wfstLine,
+                                                       {
+                                                       id:"line",
+                                                       standalone:true,
+                                                       mode: OpenLayers.Control.ModifyFeature.RESHAPE | OpenLayers.Control.ModifyFeature.DRAG// | OpenLayers.Control.ModifyFeature.RESIZE | OpenLayers.Control.ModifyFeature.ROTATE
+                                                       });
+    var editPolygon = new OpenLayers.Control.ModifyFeature(wfstPolygon,
+                                                           {
+                                                           id:"polygon",
+                                                           standalone:true,
+                                                           mode: OpenLayers.Control.ModifyFeature.RESHAPE | OpenLayers.Control.ModifyFeature.DRAG// | OpenLayers.Control.ModifyFeature.RESIZE | OpenLayers.Control.ModifyFeature.ROTATE
+                                                           });
+
+    map.addControls([editPoint, editLine, editPolygon]);
+
+
+    var select = new OpenLayers.Control.SelectFeature(
+                                                      [wfstPoint, wfstLine, wfstPolygon],
+                                                      {
+                                                      title : "Modify Feature",
+                                                      displayClass: "olControlModifyFeature",
+                                                      hover:true,
+                                                      callbacks:{
+                                                      'click':function(feature) {
+                                                      editPoint.deactivate();
+                                                      editLine.deactivate();
+                                                      editPolygon.deactivate();
+                                                      
+                                                      feature.layer.map.getControl(feature.layer.id).activate();
+                                                      feature.layer.map.getControl(feature.layer.id).selectFeature(feature);
+                                                      },
+                                                      'clickout':function(feature) {
+                                                      feature.layer.map.getControl(feature.layer.id).deactivate();
+                                                      }
+                                                      }
+                                                      });
+
+    var save = new OpenLayers.Control.Button({
+                                             title: "Save Changes",
+                                             trigger: function() {
+                                             /*if(edit.feature) {
+                                              edit.selectControl.unselectAll();
+                                              }*/
+                                             editPoint.deactivate();
+                                             editLine.deactivate();
+                                             editPolygon.deactivate();
+                                             
+
+                                            if (mc.mode == 'zone') {
+                                              savePolygon.save(); 
+                                              log('Saved Zones');
+                                            } else {
+                                              savePoint.save();
+                                              saveLine.save();
+                                              log('Saved Points');
+                                            }
+                                             
+                                             
+                                             },
+                                             displayClass: "olControlSaveFeatures"
+                                             });
+
+
+    var del = new DeleteFeature([wfstPoint, wfstLine, wfstPolygon], {title: "Delete Feature", hover:true});
+
+    panel.addControls([save, del, select, point, path, polygon]);
+    map.addControl(panel);
+
+    var styledMapOptions = {
+        name: "Styled Map"
+    };
+
+    vectors = new OpenLayers.Layer.Vector("Vector Layer", {displayInLayerSwitcher: false});
+
+
+    //setup geoserver
+    this.tiled = tiled = new OpenLayers.Layer.WMS(
         "Siam", 
         "http://115.146.85.81:8080/geoserver/it.geosolutions/wms",
         {
@@ -44,33 +312,18 @@ function setupMap() {
             isBaseLayer: false,
             yx : {'EPSG:4326' : true}
         } 
-    );*/
-    var tiled = new OpenLayers.Layer.Vector("WFS (read only)", {
-        strategies: [new OpenLayers.Strategy.BBOX()],
-        protocol: new OpenLayers.Protocol.WFS({
-            version: "1.1.0",
-            srsName: "EPSG:4326",
-            url:  "http://115.146.85.81:9090/fs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=test",
-            featurePrefix: "fs",
-            featureType: "test",
-            featureNS: "http://example.com/featureserver"
-         })
-     });
+    );
+
+    map.addLayer(tiled);
 
 
-    var styledMapOptions = {
-        name: "Styled Map"
-    };
-
-    vectors = new OpenLayers.Layer.Vector("Vector Layer", {displayInLayerSwitcher: false});
 
     var styledMapType = new google.maps.StyledMapType(gmStyle, styledMapOptions);
 
-    //map.addLayers([gmap,osm,gsat,tiled]);
-    map.addLayers([tiled]);
+    map.addLayers([gmap,gsat,osm]);
 
     map.addControl(new OpenLayers.Control.LayerSwitcher());
-    map.addControl(new OpenLayers.Control.EditingToolbar(vectors));
+    //map.addControl(new OpenLayers.Control.EditingToolbar(vectors));
 
     map.setCenter(
         new OpenLayers.LonLat(145.69826, -17.37545).transform(
@@ -85,7 +338,37 @@ function setupMap() {
 
     gsat.mapObject.mapTypes.set('styled', styledMapType);
     gsat.mapObject.setMapTypeId('styled');
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //google map style
     gmStyle = [
